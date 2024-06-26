3f0895c05d1424c498da706d40a6a62ad28d4c6f
devxprite <devxprite@gmail.com>
2024-06-26 12:36:13 +0530

    Add CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // turn off strict mode
     reactStrictMode: false,
+
+    // cors
+    async headers(){
+        return [
+            {
+                source: "/api/(.*)",
+                headers: [
+                    {
+                      key: "Access-Control-Allow-Origin",
+                      value: "*", // Set your origin
+                    },
+                    {
+                      key: "Access-Control-Allow-Methods",
+                      value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                      key: "Access-Control-Allow-Headers",
+                      value: "Content-Type, Authorization",
+                    },
+                  ],
+            }
+        ]
+    }
 };
 
 export default nextConfig;
